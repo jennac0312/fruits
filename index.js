2b6d468e6c977353d04b7827ed5b5825b2f4d520
Jenna C <jennac0312@gmail.com>
2023-08-08 14:05:47 -0400

    fruit params

--- index.js
+++ index.js
@@ -14,12 +14,21 @@ app.get('/fruits', ( req, res ) => {
 
 
 
+// best practice keep this at end... 
+// more specific roots come first!!
+app.get('/fruits/:indexOfFruitsArray', ( req, res ) => {
+    res.send( fruits[req.params.indexOfFruitsArray] )
+})
 
 
 
 
 
 
+
+
+
+// keep at bottom
 app.listen(PORT, ( req, res ) => {
     console.log(`Server is running on port ${PORT}. Better go catch it!`)
 })
